170048c118f7287ea1900e6d512b60fc084169b4
Hector <adeniyi.olaitanhector@yahoo.com>
2022-09-27 18:11:27 +0100

    setup home

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import { createBrowserRouter as RouterProvider, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
